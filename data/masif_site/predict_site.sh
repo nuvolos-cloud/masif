4c79c443c6a156d329611582bbc181f7e9d23a62
Daniel Sali <daniel.sali@alphacruncher.com>
2024-06-06 08:38:52 +0000

    Add re-trained model, update prediction/color_site scripts to use new model

--- data/masif_site/predict_site.sh
+++ data/masif_site/predict_site.sh
@@ -4,4 +4,4 @@ masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-python -W ignore $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l_v2.custom_params $1 $2
+python -W ignore $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l_v3.custom_params $1 $2
