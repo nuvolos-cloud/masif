1b381ee28adc50d0a6ea847b79486e09edac499f
Daniel Sali <daniel.sali@alphacruncher.com>
2024-08-16 12:13:26 +0200

    ppi_search cache reshape

--- source/masif_ppi_search/masif_ppi_search_cache_training_data.py
+++ source/masif_ppi_search/masif_ppi_search_cache_training_data.py
@@ -181,7 +181,9 @@ if not os.path.exists(params["cache_dir"]):
     os.makedirs(params["cache_dir"])
 
 binder_rho_wrt_center = np.concatenate(binder_rho_wrt_center, axis=0)
-binder_theta_wrt_center = np.concatenate(binder_theta_wrt_center, axis=0)
+binder_theta_wrt_center = np.concatenate(binder_theta_wrt_center, axis=0).reshape(
+    -1, binder_rho_wrt_center.shape[1]
+)
 binder_input_feat = np.concatenate(binder_input_feat, axis=0)
 binder_mask = np.concatenate(binder_mask, axis=0)
 
